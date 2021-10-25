c1ca651767ed71e94323cbf98b98774f14919883
Micaela Casais <casaismicaela@gmail.com>
2021-10-24 23:42:51 -0300

    BASD: modal div added to subscription.html, respectives styles added to css file, and modal functions and fetch added to js file

--- js/subscription.js
+++ js/subscription.js
@@ -32,9 +32,12 @@ var divsErrorMsg = document.getElementsByClassName('errosMsg')
 //Button
 var sendBtn = document.getElementsByClassName('submitBtn');
 
-//Validations Div
-var validations = document.getElementById('validationsDiv');
-var validationsList = document.getElementById('validationsList');
+//Modal Div NEW
+var modalDiv = document.getElementById('modalDiv');
+var modalTitle = document.getElementById('modalTitle');
+var modalData = document.getElementById('modalP')
+var closeBtn = document.getElementById('closeBtn');
+
 
 //Regular Expressions
 var nameReg = /^[a-zA-ZáéíóúÑñ]+(?:\s[a-zA-ZáéíóúÑñ]+)+$/;
@@ -67,10 +70,12 @@ function nameValidation() {
         nameInput.style.border = '2px solid green';
         nameMsg.style.display = 'none';
         fields['name'] = true;
+        return true;
     } else {
         nameInput.style.border = '2px solid red';
         nameMsg.style.display = 'block';
         fields['name'] = false;
+        return false;
     }
 }
 nameInput.addEventListener('blur', nameValidation);
@@ -86,10 +91,12 @@ function emailValidation() {
         emailInput.style.border = '2px solid green';
         emailMsg.style.display = 'none';
         fields['email'] = true;
+        return true;
     } else {
         emailInput.style.border = '2px solid red';
         emailMsg.style.display = 'block';
         fields['email'] = false;
+        return false;
     }
 }
 emailInput.addEventListener('blur', emailValidation);
@@ -105,10 +112,12 @@ function passwordValidation() {
         passwordInput.style.border = '2px solid green';
         passwordMsg.style.display = 'none';
         fields['password'] = true;
+        return true;
     } else {
         passwordInput.style.border = '2px solid red';
         passwordMsg.style.display = 'block';
         fields['password'] = false;
+        return false;
     }
 }
 passwordInput.addEventListener('blur', passwordValidation);
@@ -124,10 +133,12 @@ function confirmPassValidation(){
         confirmPasswordInput.style.border = '2px solid green';
         confirmPasswordMsg.style.display = 'none';
         fields['password2'] = true;
+        return true;
     }else{
         confirmPasswordInput.style.border = '2px solid red';
         confirmPasswordMsg.style.display = 'block';
         fields['password2'] = false;
+        return false;
     }
 }
 confirmPasswordInput.addEventListener('blur', confirmPassValidation);
@@ -146,10 +157,12 @@ function ageValidation () {
         ageInput.style.border = '2px solid green';
         ageMsg.style.display = 'none';
         fields['age'] = true;
+        return true;
     } else {
         ageInput.style.border = '2px solid red';
         ageMsg.style.display = 'block';
         fields['age'] = false;
+        return false;
     }
 }
 ageInput.addEventListener('blur', ageValidation);
@@ -169,10 +182,12 @@ function phoneNumValidation () {
         phoneNumInput.style.border = '2px solid green';
         phoneNumMsg.style.display = 'none';
         fields['phoneNumber'] = true;
+        return true;
     } else {
         phoneNumInput.style.border = '2px solid red';
         phoneNumMsg.style.display = 'block';
         fields['phoneNumber'] = false;
+        return false;
     }
 }
 phoneNumInput.addEventListener('blur', phoneNumValidation);
@@ -191,10 +206,12 @@ function addressValidation () {
         addressInput.style.border = '2px solid green';
         addressMsg.style.display = 'none';
         fields['address'] = true;
+        return true;
     } else {
         addressInput.style.border = '2px solid red';
         addressMsg.style.display = 'block';
         fields['address'] = false;
+        return false;
     }
 }
 addressInput.addEventListener('blur', addressValidation);
@@ -210,10 +227,12 @@ function cityValidation () {
         cityInput.style.border = '2px solid green';
         cityMsg.style.display = 'none';
         fields['city'] = true;
+        return true;
     } else {
         cityInput.style.border = '2px solid red';
         cityMsg.style.display = 'block';
         fields['city'] = false;
+        return false;
     }
 }
 cityInput.addEventListener('blur', cityValidation);
@@ -229,10 +248,12 @@ function postCodeValidation () {
         postCodeInput.style.border = '2px solid green';
         postCodeMsg.style.display = 'none';
         fields['postCode'] = true;
+        return true;
     } else {
         postCodeInput.style.border = '2px solid red';
         postCodeMsg.style.display = 'block';
         fields['postCode'] = false;
+        return false;
     }
 }
 postCodeInput.addEventListener('blur', postCodeValidation);
@@ -252,10 +273,12 @@ function dniValidation () {
         dniInput.style.border = '2px solid green';
         dniMsg.style.display = 'none';
         fields['dni'] = true;
+        return true;
     } else {
         dniInput.style.border = '2px solid red';
         dniMsg.style.display = 'block';
         fields['dni'] = false;
+        return false
     }
 }
 dniInput.addEventListener('blur', dniValidation);
@@ -269,31 +292,130 @@ dniInput.addEventListener('focus', clearDniMsg);
 registerForm.onsubmit= function(e) {
     e.preventDefault();
     if (
-        fields ['name'] && 
-        fields ['email'] && 
-        fields ['password'] && 
-        fields ['password2'] && 
-        fields ['age'] && 
-        fields ['phoneNumber'] && 
-        fields ['address'] && 
-        fields ['city'] && 
-        fields ['postCode'] && 
-        fields ['dni'] 
-    ) {
-        validations.style.display = 'flex';
-        alert ('Subscription process successful. Your user data is:' + '\n' +
-        'Name: ' + nameInput.value + '\n' +
-        'Email: ' + emailInput.value + '\n' +
-        'Age: ' + ageInput.value + '\n' +
-        'Phone Number: ' + phoneNumInput.value + '\n' +
-        'Address: ' + addressInput.value + '\n' +
-        'City: ' + cityInput.value + '\n' +
-        'Post Code: ' + postCodeInput.value + '\n' +
-        'DNI: ' + dniInput.value)
-    } else {
-        alert ('Subscription process failed. Please check your data and try again.')
+        nameValidation() == true  && 
+        emailValidation() == true  && 
+        passwordValidation() == true  && 
+        confirmPassValidation() == true  && 
+        ageValidation() == true  && 
+        phoneNumValidation() == true  && 
+        addressValidation() == true && 
+        cityValidation() == true  && 
+        postCodeValidation() == true  && 
+        dniValidation() == true
+        ) {
+        modalMsg();
+        } else { 
+        showErrorMsg () 
+        }
+}
+
+//Saving input's data into Local Storage 
+
+function localStorageData() {
+    localStorage.setItem('name', nameInput.value);
+    localStorage.setItem('email', emailInput.value);
+    localStorage.setItem('password', passwordInput.value);
+    localStorage.setItem('confirmPassword', confirmPasswordInput.value);
+    localStorage.setItem('age', ageInput.value);
+    localStorage.setItem('phone', phoneNumInput.value);
+    localStorage.setItem('address', addressInput.value);
+    localStorage.setItem('city', cityInput.value);
+    localStorage.setItem('postalCode', postCodeInput.value);
+    localStorage.setItem('DNI', dniInput.value);
+};
+
+//Modal Div with Successful Info
+function modalDivSuccess(userInfo) {
+    modalDiv.style.display = 'block';
+    modalTitle.innerText = 'Subscription process has been susccessful! \n Your data is:';
+    modalData.innerText = 
+        `Name: ${userInfo.name} \n
+        Email: ${userInfo.email} \n
+        Password: ${userInfo.password} \n
+        Confirm Password: ${userInfo.confirmPassword} \n
+        Age: ${userInfo.age} \n
+        Phone Number: ${userInfo.phone} \n
+        Address: ${userInfo.address} \n
+        City: ${userInfo.city} \n
+        Postal Code: ${userInfo.postalCode} \n
+        DNI: ${userInfo.dni} \n
+        `
+};
+
+//Modal Div with Error info
+function modalDivErr(errorInfo) {
+    modalDiv.style.display = 'block';
+    modalTitle.innerHTML = 'Subscription process failed. Please check your data and try again.';
+    modalData.innerHTML = `<p>${errorInfo}</p>`;
+};
+
+//Error message for submit fn, if validations don't pass
+function showErrorMsg () {
+    modalDiv.style.display = 'block';
+    modalTitle.innerHTML = 'Subscription process failed';
+    modalData.innerText = 'Please check your data and try again';
+}
+
+//Modal with fetch call
+function modalMsg(e) {
+    var url = 'http://curso-dev-2021.herokuapp.com/newsletter?';
+    var queryParams = `name=${nameInput.value}&email=${emailInput.value}
+    &password=${passwordInput.value}&confirmPassword=${confirmPasswordInput.value}&age=${ageInput.value}&phone=${phoneNumInput.value}
+    &address=${addressInput.value}&city=${cityInput.value}&postalCode=${postCodeInput.value}
+    &dni=${dniInput.value}`;
+    fetch(`${url}${queryParams}`)
+        .then(response =>
+            response.json())
+        .then(data => {
+            modalDivSuccess(data)
+            localStorageData()
+        })
+        .catch((error) => {
+            modalDivErr(error);
+        });
+    e.preventDefault();
+};
+
+// Fill the inputs with saved data of local storage
+function savedDataLS() {
+    var currentName = localStorage.getItem('name');
+    var currentEmail = localStorage.getItem('email');
+    var currentPassword = localStorage.getItem('password');
+    var currentPassword2 = localStorage.getItem('confirmPassword');
+    var currentAge = localStorage.getItem('age');
+    var currentPhoneNum = localStorage.getItem('phone');
+    var currentAddress = localStorage.getItem('address');
+    var currentCity = localStorage.getItem('city');
+    var currentPostalCode = localStorage.getItem('postalCode');
+    var currentDNI = localStorage.getItem('DNI');
+
+    nameInput.value = currentName;
+    emailInput.value = currentEmail;
+    passwordInput.value = currentPassword;
+    confirmPasswordInput.value = currentPassword2;
+    ageInput.value = currentAge;
+    phoneNumInput.value = currentPhoneNum;
+    addressInput.value = currentAddress;
+    cityInput.value = currentCity;
+    postCodeInput.value = currentPostalCode;
+    dniInput.value = currentDNI;
+}
+
+window.addEventListener('load', savedDataLS);
+
+// Closing Modal Div
+function closeModal() {
+    modalDiv.style.display = 'none';
+}
+closeBtn.addEventListener('click', closeModal);
+
+function outsideClick(e) {
+    if (e.target == modalDiv) {
+        modalDiv.style.display = 'none';
     }
 }
+window.addEventListener('click', outsideClick)
+
 // Bonus task
 
 var bonusDiv = document.getElementById('bonus');
@@ -303,45 +425,4 @@ function fillingDiv() {
         bonusDiv.innerHTML = 'Welcome ' + nameInput.value;
     }
 }
-nameInput.addEventListener('keydown',fillingDiv);
-
-
-/*
-// validations div code, not working
-
-
-//list Items of Validations Div
-var li = document.createElement("li");
-var content = document.createTextNode( 'Name: ', nameInput.value);
-li.appendChild(content);
-
-var li2 = document.createElement("li");
-var content2 = document.createTextNode("Email: " + emailInput.value);
-li2.appendChild(content2);
-
-var li3 = document.createElement("li");
-var content3 = document.createTextNode("Age: " + ageInput.value);
-li3.appendChild(content3); 
-
-var li4 = document.createElement("li");
-var content4 = document.createTextNode("PhoneNumber: " + phoneNumInput.value);
-li4.appendChild(content4);
-
-var li5 = document.createElement("li");
-var content5 = document.createTextNode("Address: " + addressInput.value);
-li5.appendChild(content5);
-
-var li6 = document.createElement("li");
-var content6 = document.createTextNode("City: " + cityInput.value);
-li6.appendChild(content6);
-
-var li7 = document.createElement("li");
-var content7 = document.createTextNode("Post Code: " + postCodeInput.value);
-li7.appendChild(content7);
-
-var li8 = document.createElement("li");
-var content8 = document.createTextNode("DNI: " + dniInput.value);
-li8.appendChild(content8);
-
-validationsList.append(li,li2,li3,li4,li5,li6,li7,li8);
-*/
+nameInput.addEventListener('keyup',fillingDiv);
